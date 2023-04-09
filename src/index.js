f76c85e906e8aa399f553a7412a5b56b6a8a9db4
malabananangelo21 <49380705+malabananangelo21@users.noreply.github.com>
2023-04-09 16:50:52 +0800

    Update Index.js(Configure Auth0)
    Configure the auth0 authentication for login

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Auth0Provider
+    domain="dev-ylhgj4ys0f3fga1w.us.auth0.com"
+    clientId="p1ciIguK2WlqSZ7NFhhnmwFE4OePTFOe"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+    useRefreshTokens
+    cacheLocation="localstorage"
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
