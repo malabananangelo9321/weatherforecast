c8ace0b12260f493a0a073f3357ce01a2f456268
malabananangelo21 <49380705+malabananangelo21@users.noreply.github.com>
2023-04-09 14:43:47 +0800

    Create Search Component
    Create reusable search component

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./css/search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/fontawesome-free-solid";
+const renderEqualProps = (prevProps, nextProps) => {
+  return JSON.stringify(prevProps) === JSON.stringify(nextProps);
+};
+const Search = (props) => {
+  const { value, onChangeText, name } = props;
+  return (
+    <div className="search-container">
+      <FontAwesomeIcon icon={faSearch} className="input-icon-search" />
+      <input
+        className="input-search"
+        value={value}
+        onChange={onChangeText}
+        name={name}
+      />
+    </div>
+  );
+};
+
+export default React.memo(Search, renderEqualProps);
