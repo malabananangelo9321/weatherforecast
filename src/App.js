834ecbd74f43bf7da0af3998cc01d95eeebbdeba
malabananangelo21 <49380705+malabananangelo21@users.noreply.github.com>
2023-04-09 16:52:21 +0800

    Update App.js (Configure React Router Dom)
    Configure react-router-dom for page navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import "./App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import IndexLandingPage from "./pages/landing/pageComponents/IndexLandingPage";
 import TopNavigation from "./components/navigation/TopNavigation";
+import { useAuth0 } from "@auth0/auth0-react";
+import IndexHomePage from "./pages/home/pageComponents/IndexHomePage";
+import IndexWeatherPage from "./pages/weather/pageComponents/IndexWeatherPage";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<TopNavigation />}>
-          <Route path="/" element={<IndexLandingPage />} />
+          {!isAuthenticated ? (
+            <Route path="/" element={<IndexLandingPage />} />
+          ) : (
+            <>
+              <Route path="/" element={<IndexHomePage />} />
+              <Route path="/home" element={<IndexHomePage />} />
+              <Route path="/weather/:search" element={<IndexWeatherPage />} />
+            </>
+          )}
         </Route>
       </Routes>
     </HashRouter>
