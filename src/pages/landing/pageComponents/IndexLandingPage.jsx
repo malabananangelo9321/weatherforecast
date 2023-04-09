4536548afa58b2d6cdd3f1a6090487941661126a
malabananangelo21 <49380705+malabananangelo21@users.noreply.github.com>
2023-04-09 14:51:23 +0800

    Index Landing Page (Add Login Button and Hello World String Display)
    Add login button and and add redirect  uri after login authentication

--- src/pages/landing/pageComponents/IndexLandingPage.jsx
+++ src/pages/landing/pageComponents/IndexLandingPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { useAuth0 } from "@auth0/auth0-react";
 import "../css/landing.css";
 const IndexLandingPage = () => {
+  const { loginWithRedirect } = useAuth0();
+  const onLogin = async () => {
+    await loginWithRedirect({
+      authorizationParams: {
+        redirect_uri: "http://localhost:3000/#/home",
+      },
+    });
+  };
   return (
     // * LANDING CONTAINER START *
     <div className="landing-container">
@@ -9,6 +17,7 @@ const IndexLandingPage = () => {
       <div className="landing-item">
         {/* WELCOME GREETING ITEM START */}
         <div>
+          <p>Hello World!</p>
           <p>
             Welcome to the weather forecast web application. Please login with
             your
@@ -20,7 +29,9 @@ const IndexLandingPage = () => {
         {/* WELCOME GREETING ITEM END */}
         {/* LOGIN ITEM BUTTON START */}
         <div>
-          <button className="button-style">Login</button>
+          <button className="button-style" onClick={onLogin}>
+            Login
+          </button>
         </div>
         {/* LOGIN ITEM BUTTON END */}
       </div>
