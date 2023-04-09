49a8365a3b7f7292eea1481e5632e5366f0517d5
malabananangelo21 <49380705+malabananangelo21@users.noreply.github.com>
2023-04-09 14:42:51 +0800

    Top Navigation (Create Logout)
    Create logout button and onLogout function.

--- src/components/navigation/TopNavigation.jsx
+++ src/components/navigation/TopNavigation.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import "./css/topNav.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCloud } from "@fortawesome/fontawesome-free-solid";
+import { faCloud, faPowerOff } from "@fortawesome/fontawesome-free-solid";
+import { useAuth0 } from "@auth0/auth0-react";
 const TopNavigation = () => {
+  const { isAuthenticated, logout } = useAuth0();
+  const onLogout = () => {
+    logout(window.location.origin + "/#/");
+    console.log(window.location.origin);
+  };
+
   return (
     <div>
       <div className="top-nav">
-        <FontAwesomeIcon icon={faCloud} className="top-icon" />
-        <h2>Weather Forcast</h2>
+        <div className="top-nav-item">
+          <FontAwesomeIcon icon={faCloud} className="top-icon" />
+          <h2>Weather Forcast</h2>
+        </div>
+
+        <div className="top-nav-item">
+          {isAuthenticated ? (
+            <div onClick={onLogout} className="button-logout">
+              <FontAwesomeIcon icon={faPowerOff} />
+            </div>
+          ) : null}
+        </div>
       </div>
       <div className="container">
         <Outlet />
