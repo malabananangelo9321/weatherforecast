6b70e9d413e16d5ca659d6d67bd5448aa128237f
malabananangelo21 <49380705+malabananangelo21@users.noreply.github.com>
2023-04-09 16:41:57 +0800

    TopNavigation.jsx (Create Navigation Display)
    Create navigation display

--- src/components/navigation/TopNavigation.jsx
+++ src/components/navigation/TopNavigation.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import "./css/topNav.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloud, faPowerOff } from "@fortawesome/fontawesome-free-solid";
 import { useAuth0 } from "@auth0/auth0-react";
+
 const TopNavigation = () => {
+  const navigate = useNavigate();
   const { isAuthenticated, logout } = useAuth0();
   const onLogout = () => {
     logout(window.location.origin + "/#/");
-    console.log(window.location.origin);
   };
+  const onRoute = (route) => {
+    let newRoute = route;
+    // Add String /default Parameter in URL if route is /weather
+    if (newRoute == "/weather") {
+      newRoute = `${route}/default`;
+    }
+    navigate(newRoute);
+  };
+  const navigations = isAuthenticated
+    ? [
+        { name: "Home", route: "/" },
+        { name: "Weather", route: "/weather" },
+      ]
+    : [];
 
   return (
     <div>
@@ -20,6 +35,15 @@ const TopNavigation = () => {
         </div>
 
         <div className="top-nav-item">
+          <ul>
+            {navigations.map((val) => {
+              return (
+                <li key={val.name} onClick={() => onRoute(val.route)}>
+                  <a className="nav-color">{val.name}</a>
+                </li>
+              );
+            })}
+          </ul>
           {isAuthenticated ? (
             <div onClick={onLogout} className="button-logout">
               <FontAwesomeIcon icon={faPowerOff} />
